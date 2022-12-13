63e487668e1bd68e5346ba19fc82b0246744dd65
hongxiang.gao <hongxiang.gao@retech.work>
2022-12-14 02:27:42 +0800

    feat:原来多个节点，现在只有一个节点

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,17 @@ function FunctionComponent() {
   const [number, setNumber] = React.useState(0)
   return number === 0 ? (
     <ul key="container" onClick={() => setNumber(number + 1)}>
-      <li key="A" id="A">
-        A
-      </li>
+      <li key="A">A</li>
       <li key="B" id="B">
         B
       </li>
-      <li key="C" id="C">
-        C
-      </li>
+      <li key="C">C</li>
     </ul>
   ) : (
     <ul key="container" onClick={() => setNumber(number + 1)}>
-      <li key="A" id="A2">
-        A2
+      <li key="B" id="B2">
+        B2
       </li>
-      <p key="B" id="B">
-        B
-      </p>
     </ul>
   )
 }
