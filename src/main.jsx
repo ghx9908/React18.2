bbdec36c3c41a28dd4397cbc0a2416ef89d28aa7
hongxiang.gao <hongxiang.gao@retech.work>
2022-12-14 02:22:07 +0800

    feat:原来多个节点，现在只有一个节点

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,30 @@ import * as React from "react"
 import { createRoot } from "react-dom/client"
 
 function FunctionComponent() {
-  console.log("FunctionComponent")
   const [number, setNumber] = React.useState(0)
-  //如果使用的是useState，调用setNumber的时候传入的是老状态，则不需要更新，
   return number === 0 ? (
-    <div
-      key="title1"
-      id="title"
-      onClick={() => {
-        setNumber(number + 1)
-      }}
-    >
-      title
-    </div>
+    <ul key="container" onClick={() => setNumber(number + 1)}>
+      <li key="A" id="A">
+        A
+      </li>
+      <li key="B" id="B">
+        B
+      </li>
+      <li key="C" id="C">
+        C
+      </li>
+    </ul>
   ) : (
-    <p
-      key="title1"
-      id="title2"
-      onClick={() => {
-        setNumber(number + 1)
-      }}
-    >
-      title2
-    </p>
+    <ul key="container" onClick={() => setNumber(number + 1)}>
+      <li key="A" id="A2">
+        A2
+      </li>
+      <p key="B" id="B">
+        B
+      </p>
+    </ul>
   )
 }
 let element = <FunctionComponent />
-//old let element = React.createElement(FunctionComponent);
-//new let element = jsx(FunctionComponent);
 const root = createRoot(document.getElementById("root"))
-//把element虚拟DOM渲染到容器中
 root.render(element)
-
-//老fiber树 div#root对应的fiber.child=FunctionComponent的fiber.child=button.fiber
